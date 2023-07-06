8360db03b43c3b517e17f22dcd2031b01aacb3b0
Tomson331 <133116477+Tomson331@users.noreply.github.com>
2023-07-06 11:20:31 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Day 6 - String
+
+print(12 + 12)
+print("12" + "12")
+
+print("12" + 12)
+
+# To jest komentarz
+print("Zero To Junior Forever :)")
+
+# To też jest komentarz
+# TODO
+print("To się na pewno nie wyświetli") # no to komentujemy
+# dalsza część komentarza
+
+
